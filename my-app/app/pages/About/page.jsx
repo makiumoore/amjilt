bbcf35fc3af776e8cb70fdd5cf65f8e215f48ca2
beku <146428492+Bekupollll@users.noreply.github.com>
2025-02-07 17:42:39 +0800

    pzda

--- my-app/app/pages/About/page.jsx
+++ my-app/app/pages/About/page.jsx
@@ -1,7 +1,10 @@
+import Section1 from '@/app/components/About/Section1'
 import React from 'react'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div>page
+      <Section1/>
+    </div>
   )
 }
