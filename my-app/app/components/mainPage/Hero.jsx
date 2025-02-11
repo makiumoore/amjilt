b0249d267eebdf5a960fe657d591542c6c51466a
amraaae <AMRAAAE6@GMAIL.COM>
2025-02-11 17:47:51 +0800

    hero laptop update

--- my-app/app/components/mainPage/Hero.jsx
+++ my-app/app/components/mainPage/Hero.jsx
@@ -3,13 +3,14 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div className='w-full p-12'>
-      <div className='w-min_width lg:w-max_width h-0.5  '>
+    <div className='w-min_width lg:w-max_width m-auto pt-12 flex flex-col items-center justify-center gap-10'>
+      <div className=''>
         <h1 className='text-5xl font-bold text-center py-8'>Amjilt Learning Management System</h1>
         <p className='text-2xl text-center'>Сургуулийн удирдлагын цогц систем</p>
       </div>
-      <div className='flex justify-center'>
-        <Image width={1200} height={500} src="/hero/laptop_realistic.png" alt="amjilt_icon" />
+      <div className='flex justify-center w-[1000px]'>
+        {/* <Image width={1200} height={500} src="/hero/laptop_realistic.png" alt="amjilt_icon" /> */}
+        <img src="/hero/laptop_realistic.png" alt="" className='w-full h-[550px]' />
       </div>
     </div>
   )
