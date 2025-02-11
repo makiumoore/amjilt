0c06d5564a059249ae5e6d82627af82694147317
beku <146428492+Bekupollll@users.noreply.github.com>
2025-02-11 17:53:32 +0800

    header update

--- my-app/app/components/mainPage/Header.jsx
+++ my-app/app/components/mainPage/Header.jsx
@@ -11,14 +11,14 @@ export default function Header() {
             <img src="/header_icons/amjilt.svg" alt="amjilt_icon" className='w-full h-full' />
           </div>
           <div className='flex items-center gap-14 ml-10'>
-            <Link href={'/pages/About'}><p className='font-medium text-md poppins-2xl text-gray-700'>Бидний тухай</p></Link>
-            <Link href={'/pages/System'}><p className='font-medium text-md poppins-2xl text-gray-700'>Cестим</p></Link>
+            <Link href={'/pages/About'}><p className='font-medium text-md poppins-2xl text-gray-700 hover:text-second_color duration-300'>Бидний тухай</p></Link>
+            <Link href={'/pages/System'}><p className='font-medium text-md poppins-2xl text-gray-700 hover:text-second_color duration-300'>Cистем</p></Link>
           </div>
         </div>
         <div className='col-span-1 flex items-center justify-end gap-10'>
-          <p className='font-medium text-md poppins-2xl text-gray-700'>Тусламж</p>
+          <p className='font-medium text-md poppins-2xl text-gray-700 hover:text-second_color cursor-pointer duration-300'>Тусламж</p>
           <div>
-            <button className='bg-second_color shadow-md rounded-md py-2 px-4 flex items-center gap-2'>
+            <button className='bg-second_color hover:bg-purple-500 duration-300 shadow-md rounded-md py-2 px-4 flex items-center gap-2 transition duration-300 ease-in-out'>
               <p className='font-bold poppins-2xl text-md bg-transparent text-white'>Холбогдох</p>
               <img src="/header_icons/Vector.svg" alt="Union.svg" className=''/>
             </button>
