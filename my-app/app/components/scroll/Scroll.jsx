f237549d7e09f40bd3b0601081600f9234aebcb4
beku <146428492+Bekupollll@users.noreply.github.com>
2025-02-12 16:48:39 +0800

    scroll update

--- my-app/app/components/scroll/Scroll.jsx
+++ my-app/app/components/scroll/Scroll.jsx
@@ -3,11 +3,11 @@ import Marquee from "react-fast-marquee";
 
 export default function Scroll() {
   return (
-    <div className="w-full h-screen ">
-  <Marquee  speed={100} gradient={false} className="overflow-y-hidden">
-    <h1 className="text-8xl font-bold">scroll</h1>
-  </Marquee>
-</div>
+    <div className="w-full h-auto py-10 bg-gray-100">
+    <Marquee  speed={100} gradient={false} className="overflow-y-hidden">
+      <img src="/amjilt.svg" alt=""  className='w-[18%] pl-10'/>
+    </Marquee>
+  </div>
 
 
   )
