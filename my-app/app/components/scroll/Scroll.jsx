11b789158943cd98323ac567d94571cc0be00743
amraaae <AMRAAAE6@GMAIL.COM>
2025-02-12 17:02:30 +0800

    scroll logo update

--- my-app/app/components/scroll/Scroll.jsx
+++ my-app/app/components/scroll/Scroll.jsx
@@ -3,11 +3,14 @@ import Marquee from "react-fast-marquee";
 
 export default function Scroll() {
   return (
-    <div className="w-full h-auto py-10 bg-gray-100">
-    <Marquee  speed={100} gradient={false} className="overflow-y-hidden">
-      <img src="/amjilt.svg" alt=""  className='w-[18%] pl-10'/>
-    </Marquee>
-  </div>
+    <div className="w-full h-auto bg-gray-100 flex items-center">
+      <Marquee pauseOnHover={false} speed={80} gradient={false}>
+        <img src="/amjilt.svg" alt="Image 1" className="w-40 h-40 mx-12 rounded-lg" />
+        <img src="/amjilt.svg" alt="Image 2" className="w-40 h-40 mx-12 rounded-lg" />
+        <img src="/amjilt.svg" alt="Image 3" className="w-40 h-40 mx-12 rounded-lg" />
+        <img src="/amjilt.svg" alt="Image 4" className="w-40 h-40 mx-12 rounded-lg" />
+      </Marquee>
+    </div>
 
 
   )
