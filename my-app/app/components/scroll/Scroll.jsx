a2c9f2d98087c67797b042ac29e680d4bfddc0a4
amraaae <AMRAAAE6@GMAIL.COM>
2025-02-12 17:02:54 +0800

    scroll py

--- my-app/app/components/scroll/Scroll.jsx
+++ my-app/app/components/scroll/Scroll.jsx
@@ -3,7 +3,7 @@ import Marquee from "react-fast-marquee";
 
 export default function Scroll() {
   return (
-    <div className="w-full h-auto bg-gray-100 flex items-center">
+    <div className="w-full h-auto py-10 bg-gray-100 flex items-center">
       <Marquee pauseOnHover={false} speed={80} gradient={false}>
         <img src="/amjilt.svg" alt="Image 1" className="w-40 h-40 mx-12 rounded-lg" />
         <img src="/amjilt.svg" alt="Image 2" className="w-40 h-40 mx-12 rounded-lg" />
